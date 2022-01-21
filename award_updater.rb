1b2626f6fc6c6065f4a27c17fd8fed0f3794375c
Shawn Farrow <shaallfar@gmail.com>
2022-01-21 11:31:55 -0800

    added award updater class to stop passing award in every time

---
+++ award_updater.rb
@@ -0,0 +1,61 @@
+class AwardUpdater
+  attr_accessor :award
+
+  BLUECOMPARE_EXPIRATION_FIRST_STAGE = 10
+  BLUECOMPARE_EXPIRATION_SECOND_STAGE = 5
+  BLUESTAR_QUALITY_DECAY = 2
+  DEFAULT_EXPIRATION_DECAY = 1
+  DEFAULT_QUALITY_DECAY = 1
+  DEFAULT_QUALITY_INCREASE = 1
+  EXPIRATION = 0
+  MAXIMUM_QUALITY = 50
+  MINIMUM_QUALITY = 0
+
+  def initialize(award)
+    @award = award
+  end
+
+  def above_minimum_quality?
+    @award.quality > MINIMUM_QUALITY
+  end
+
+  def below_maximum_quality?
+    @award.quality < MAXIMUM_QUALITY
+  end
+
+  def below_minimum_quality?
+    @award.quality < MINIMUM_QUALITY
+  end
+
+  def below_bluecompare_expiration_first_stage?
+    @award.expires_in <= BLUECOMPARE_EXPIRATION_FIRST_STAGE
+  end
+
+  def below_bluecompare_expiration_second_stage?
+    @award.expires_in <= BLUECOMPARE_EXPIRATION_SECOND_STAGE
+  end
+
+  def decrease_bluestar_quality
+    @award.quality -= BLUESTAR_QUALITY_DECAY
+  end
+
+  def decrease_expiration
+    @award.expires_in -= DEFAULT_EXPIRATION_DECAY
+  end
+
+  def decrease_quality
+    @award.quality -= DEFAULT_QUALITY_DECAY
+  end
+
+  def expired?
+    @award.expires_in < EXPIRATION
+  end
+
+  def increment_quality
+    @award.quality += DEFAULT_QUALITY_INCREASE
+  end
+
+  def set_zero_quality
+    @award.quality = @award.quality - @award.quality
+  end
+end
--- update_quality.rb
+++ update_quality.rb
@@ -1,123 +1,74 @@
+require 'award_updater'
+
 class UpdateQuality
-  BLUECOMPARE_EXPIRATION_FIRST_STAGE = 10
-  BLUECOMPARE_EXPIRATION_SECOND_STAGE = 5
-  BLUESTAR_QUALITY_DECAY = 2
-  DEFAULT_EXPIRATION_DECAY = 1
-  DEFAULT_QUALITY_DECAY = 1
-  DEFAULT_QUALITY_INCREASE = 1
-  EXPIRATION = 0
-  MAXIMUM_QUALITY = 50
-  MINIMUM_QUALITY = 0
+
 
   def initialize(awards)
     @awards = awards
   end
 
   def update_quality
+
     @awards.each do |award|
+      updater = AwardUpdater.new(award)
+
       case award.name
       when 'Blue First'
-        if below_maximum_quality?(award)
-          increment_quality(award)
+        if updater.below_maximum_quality?
+          updater.increment_quality
         end
 
-        decrease_expiration(award)
+        updater.decrease_expiration
 
-        if below_minimum_quality?(award) && below_maximum_quality?(award)
-          increment_quality(award)
+        if updater.below_minimum_quality? && updater.below_maximum_quality?
+          updater.increment_quality
         end
       when 'Blue Compare'
-        if below_maximum_quality?(award)
+        if updater.below_maximum_quality?
 
-          increment_quality(award)
+          updater.increment_quality
 
-          if below_bluecompare_expiration_first_stage?(award) && below_maximum_quality?(award)
-              increment_quality(award)
+          if updater.below_bluecompare_expiration_first_stage? && updater.below_maximum_quality?
+              updater.increment_quality
           end
-          if below_bluecompare_expiration_second_stage?(award) && below_maximum_quality?(award)
-              increment_quality(award)
+          if updater.below_bluecompare_expiration_second_stage? && updater.below_maximum_quality?
+              updater.increment_quality
           end
         end
 
-        decrease_expiration(award)
+        updater.decrease_expiration
 
-        if expired?(award)
-          set_zero_quality(award)
+        if updater.expired?
+          updater.set_zero_quality
         end
 
       when 'Blue Distinction Plus'
         # never decreases in quality
       when 'Blue Star'
-        if above_minimum_quality?(award)
-          decrease_bluestar_quality(award)
+        if updater.above_minimum_quality?
+          updater.decrease_bluestar_quality
         end
 
-        decrease_expiration(award)
+        updater.decrease_expiration
 
-        if expired?(award)
-          if above_minimum_quality?(award)
-            decrease_bluestar_quality(award)
+        if updater.expired?
+          if updater.above_minimum_quality?
+            updater.decrease_bluestar_quality
           end
         end
       else
-        if above_minimum_quality?(award)
-          decrease_quality(award)
+        if updater.above_minimum_quality?
+          updater.decrease_quality
         end
 
-        decrease_expiration(award)
+        updater.decrease_expiration
 
-        if expired?(award)
-          if above_minimum_quality?(award)
-            decrease_quality(award)
+        if updater.expired?
+          if updater.above_minimum_quality?
+            updater.decrease_quality
           end
         end
       end
     end
-  end
-
-  private
-
-  def above_minimum_quality?(award)
-    award.quality > MINIMUM_QUALITY
-  end
-
-  def below_maximum_quality?(award)
-    award.quality < MAXIMUM_QUALITY
-  end
-
-  def below_minimum_quality?(award)
-    award.quality < MINIMUM_QUALITY
-  end
-
-  def below_bluecompare_expiration_first_stage?(award)
-    award.expires_in <= BLUECOMPARE_EXPIRATION_FIRST_STAGE
-  end
-
-  def below_bluecompare_expiration_second_stage?(award)
-    award.expires_in <= BLUECOMPARE_EXPIRATION_SECOND_STAGE
-  end
-
-  def decrease_bluestar_quality(award)
-    award.quality -= BLUESTAR_QUALITY_DECAY
-  end
-
-  def decrease_expiration(award)
-    award.expires_in -= DEFAULT_EXPIRATION_DECAY
-  end
-
-  def decrease_quality(award)
-    award.quality -= DEFAULT_QUALITY_DECAY
-  end
-
-  def expired?(award)
-    award.expires_in < EXPIRATION
-  end
-
-  def increment_quality(award)
-    award.quality += DEFAULT_QUALITY_INCREASE
-  end
-
-  def set_zero_quality(award)
-    award.quality = award.quality - award.quality
   end
 end
